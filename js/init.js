df58abea877cd92bed39a1dca24e1f94fcfe0233
BrunoRecagno <brunorecagno@gmail.com>
2021-11-11 12:07:29 -0300

    1

--- js/init.js
+++ js/init.js
@@ -80,23 +80,11 @@ $(window).click(function (e) {
 
 cerrarSesion = () => {
   localStorage.clear();
+
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut()
 }
 
-onSignIn = googleUser => {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-  window.location = "home.html";
-}
 
 //Event listener que se encarga de cargar el menú
 //del usuario en la barra de navegación, y también
--- js/login.js
+++ js/login.js
@@ -12,6 +12,26 @@ login = () => {
     localStorage.setItem('pass', passUsuario);
 }
 
+
+onSignIn = googleUser => {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+    
+    localStorage.setItem('nombre', profile.getName());
+    localStorage.setItem('pass', passUsuario);
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+    window.location.href = "home.html";
+  }
+
 //Función para activar el botón de login con la tecla "Enter"
 $(window).keydown(function(e) {
     if(e.code === "Enter") {
