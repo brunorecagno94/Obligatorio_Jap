85a3d69d6c2cada5c0edb086a4cedda7d747682b
BrunoRecagno <brunorecagno@gmail.com>
2021-11-11 12:29:38 -0300

    3

--- js/init.js
+++ js/init.js
@@ -78,6 +78,25 @@ $(window).click(function (e) {
   }
 })
 
+onSignIn = googleUser => {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+  
+  localStorage.setItem('nombre', profile.getName());
+  localStorage.setItem('pass', passUsuario);
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+  window.location.href = "home.html";
+}
+
 cerrarSesion = () => {
   localStorage.clear();
 
